fb04602e33966254c9b79b12edc4776da0e6f519
Aapo Mustonen <aapo.mustonen07@outlook.com>
2025-10-29 10:39:35 +0200

    first commit

---
+++ sävelmap/src/App.jsx
@@ -0,0 +1,107 @@
+import './App.css'
+
+function App() {
+  return (
+   <>
+   
+   <h1>Sävelmap</h1>
+     <svg height="1600" width="1400" xmlns="http://www.w3.org/2000/svg">
+
+        <line x1="0" y1="10" x2="100" y2="10" stroke="black" strokeWidth="3" />
+        <rect width="100" height="60" x="0" y="10" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+        <rect width="100" height="60" x="100" y="10" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="200" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="rgb(0, 0, 0)" />
+      <rect width="100" height="60" x="300" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="400" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="500" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="600" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="700" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="800" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="900" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1000" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1100" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1200" y="10" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+    
+      <line x1="0" y1="70" x2="100" y2="70" stroke="black" strokeWidth="3" />
+      <rect width="100" height="60" x="0" y="70" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="100" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="200" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="300" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="400" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="500" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="600" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="700" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="800" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="900" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1000" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1100" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1200" y="70" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+    
+      <line x1="0" y1="130" x2="100" y2="130" stroke="black" strokeWidth="3" />
+      
+      <rect width="100" height="60" x="0" y="130" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="100" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="200" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="300" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="400" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="500" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="600" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="700" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="800" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="900" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1000" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1100" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1200" y="130" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+    
+        <line x1="0" y1="190" x2="100" y2="190" stroke="black" strokeWidth="3" />
+        <rect width="100" height="60" x="0" y="190" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="100" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="200" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="300" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="400" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="500" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="600" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="700" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="800" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="900" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1000" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1100" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1200" y="190" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+    
+        <line x1="0" y1="250" x2="100" y2="250" stroke="black" strokeWidth="3" />
+        <rect width="100" height="60" x="0" y="250" fill="rgb(255, 255, 255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="100" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="200" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="300" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="400" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="500" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="600" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="700" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="800" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="900" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1000" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1100" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <rect width="100" height="60" x="1200" y="250" fill="rgb(255,255,255)" strokeWidth="3" stroke="black" />
+      <line x1="0" y1="310" x2="100" y2="310" stroke="black" strokeWidth="3" />
+      
+        <circle cx="350" cy="160" r="10" fill="black" />
+        <circle cx="550" cy="160" r="10" fill="black" />
+        <circle cx="750" cy="160" r="10" fill="black" />
+        <circle cx="950" cy="160" r="10" fill="black" />
+        <circle cx="1250" cy="160" r="10" fill="black" />
+        
+        <line x1="100" y1="0" x2="100" y2="320" stroke="black" strokeWidth="6" />
+    
+    
+       
+        
+    </svg>
+    
+   </>
+    
+
+  
+  )
+}
+
+export default App
